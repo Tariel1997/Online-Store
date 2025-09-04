92ab2b0e8c19b6fc5b6709510022be4f27611238
Tariel Khalvashi <taro.khalvashi1225@gmail.com>
2025-09-04 12:39:09 +0400

    feat(auth): Add sign out functionality and UI changes

--- js/header-scripts.js
+++ js/header-scripts.js
@@ -1,3 +1,29 @@
 function redirectToCart() {
   window.location.href = "cart.html"
 }
+
+async function signOut() {
+  const url = "https://api.everrest.educata.dev/auth/sign_out"
+
+  try {
+    const response = await axios.post(url)
+    console.log("Signed out successfully:", response.data)
+  } catch (error) {
+    console.error("Failed to sign out:", error)
+  } finally {
+    localStorage.removeItem("accessToken")
+
+    window.location.href = "./../pages/authorization.html"
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const logoutLink = document.getElementById("logout-link")
+
+  if (logoutLink) {
+    logoutLink.addEventListener("click", (event) => {
+      event.preventDefault()
+      signOut()
+    })
+  }
+})
